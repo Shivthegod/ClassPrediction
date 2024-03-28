37075937d71cb212cc77ce0f3af1dfc330fa0f3e
AIW0NG <aiden_wong@mines.edu>
2024-03-27 18:36:59 -0600

    Added CSCI403

--- RegistrationNumbers.py
+++ RegistrationNumbers.py
@@ -16,7 +16,7 @@ ffit.fit_frequentist(G, data)
 
 #Predict registration numbers
 def predict_students_registered(current_state, state_machine_graph):
-    predicted_students = {'CSCI101': 0, 'CSCI128': 0, 'CSCI200': 0, 'CSCI210': 0, 'CSCI220': 0, 'CSCI261': 0, 'CSCI262': 0, 'CSCI274': 0, 'CSCI306': 0, 'CSCI341': 0, 'CSCI358': 0, 'CSCI370': 0, 'CSCI400': 0, 'CSCI406': 0, 'CSCI442': 0, 'MATH111': 0, 'MATH112': 0, 'MATH213': 0, 'MATH332': 0, 'MATH334': 0}
+    predicted_students = {'CSCI101': 0, 'CSCI128': 0, 'CSCI200': 0, 'CSCI210': 0, 'CSCI220': 0, 'CSCI261': 0, 'CSCI262': 0, 'CSCI274': 0, 'CSCI306': 0, 'CSCI341': 0, 'CSCI358': 0, 'CSCI370': 0, 'CSCI400': 0, 'CSCI403': 0, 'CSCI406': 0, 'CSCI442': 0, 'MATH111': 0, 'MATH112': 0, 'MATH213': 0, 'MATH332': 0, 'MATH334': 0}
 
     #Loop through each edge in the state machine
     for source, target, data in state_machine_graph.edges(data=True):
